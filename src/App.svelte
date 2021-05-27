ecf96ad40aed1f57a54c3cf7f81fb69971f0ea04
chipsnet <doronomori660@outlook.jp>
2021-05-27 19:03:20 +0900

    add: とりあえず完成

--- src/App.svelte
+++ src/App.svelte
@@ -1,36 +1,29 @@
 <script>
-    export let name;
-    
-    let yourname = "";
+    let tasks = [];
+    let newTodo = "";
 
-    $: yourname
+    const remove = (i) => {
+        tasks.splice(i, 1);
+        tasks = tasks;
+    };
+
+    const add = () => {
+        tasks.push(newTodo);
+        tasks = tasks;
+        newTodo = "";
+    };
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-    <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-    <input bind:value={yourname}>
-    <p>Hello, {yourname}</p>
+    <h1>Svelte-study-todo</h1>
+
+    <input type="text" bind:value={newTodo} placeholder="ここにやることを入力してボタンをクリック" style="width: 400px;" />
+    <button on:click={add}>追加</button>
+    
+    {#each tasks as t, i}
+        <p on:click={() => remove(i)}>{t}</p>
+    {/each}
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
